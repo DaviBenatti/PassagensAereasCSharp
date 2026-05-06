da7bfa46299c2e05ff93e365765a9d86982e89e2
DaviBenatti <davilopesbenatti@gmail.com>
2026-05-06 17:19:22 -0300

    :tada: init: Commit inicial

---
+++ Models/Passagem.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PassagensAereasCSharp.Models
+{
+    public class Passagem
+    {
+        public string nomePassageiro;
+        public string origemVoo;
+        public string destinoVoo;
+        public string dataViagem;
+
+        public void exibePassagem;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace PassagensAereasCSharp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write($"Digite a senha para acessar o sistema: ");
+        string senha = Console.ReadLine();
+        
+        int opcao = 1;
+
+        do
+        {
+            if(senha == "123456")
+            {
+                Console.WriteLine($"<--- BEM VINDO AO SENAI AIRLINES --->");
+                Console.WriteLine($"[1] - Cadastrar novo passageiro");
+                Console.WriteLine($"[2] - Listagem de passagens");
+                Console.WriteLine($"[0] - Sair");
+                
+                Console.Write($"Selecione a opção: ");
+                opcao = int.Parse(Console.ReadLine());
+            }
+            else
+            {
+                while(senha != "123456")
+                {
+                    Console.Write($"Senha incorreta, digite novamente: ");
+                    senha = Console.ReadLine();
+                }
+            }    
+        } while(opcao != 0);
+    }
+}
